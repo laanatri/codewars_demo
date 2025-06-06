e9e1a781a56a7c85e8a7aa559c41bb11fd0dce46
laanatri <laanatri.ade@gmail.com>
2025-06-06 15:25:35 +0200

    add kata simple pig latin

--- JAVASCRIPT/5kyu/simple_pig_latin.js
+++ JAVASCRIPT/5kyu/simple_pig_latin.js
@@ -1,17 +1,7 @@
-// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
-
-// Examples
-// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
-// pigIt('Hello world !');     // elloHay orldway !
-
-// My solution :
-
 function pigIt(str){
-  //Code here
   const strArray = str.split(" ");
   strArray.map((word, i) => {
-    const firstLetter = word.charAt(0);
-    strArray[i] = word.substring(1) + firstLetter;
+    strArray[i] = word.substring(1) + word.charAt(0) + (strArray[i].match(/^[.,:!?]/) ? "" : "ay");
   })
-  return strArray.join("ay ") + "ay";
+  return strArray.join(" ");
 }
