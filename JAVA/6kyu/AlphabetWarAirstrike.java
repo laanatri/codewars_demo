476219bcf248c59814ae4b2f28e5dfbf4767f7bd
laanatri <laanatri.ade@gmail.com>
2025-06-18 12:17:03 +0200

    add kata alphabet war airstrike letters massacre

---
+++ JAVA/6kyu/AlphabetWarAirstrike.java
@@ -0,0 +1,82 @@
+// Introduction
+// There is a war...between alphabets!
+// There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began. The letters called airstrike to help them in war - dashes and dots are spread throughout the battlefield. Who will win?
+
+// Task
+// Write a function that accepts a fight string which consists of only small letters and * which represents a bomb drop place. Return who wins the fight after bombs are exploded. When the left side wins return Left side wins!, and when the right side wins return Right side wins!. In other cases, return Let's fight again!.
+
+// The left side letters and their power:
+
+//  w - 4
+//  p - 3 
+//  b - 2
+//  s - 1
+// The right side letters and their power:
+
+//  m - 4
+//  q - 3 
+//  d - 2
+//  z - 1
+// The other letters don't have power and are only victims.
+// The * bombs kill the adjacent letters ( i.e. aa*aa => a___a, **aa** => ______ );
+
+// Example (Input --> Output)
+// "s*zz"           --> "Right side wins!"
+// "*zd*qm*wp*bs*"  --> "Let's fight again!"
+// "zzzz*s*"        --> "Right side wins!"
+// "www*www****z"   --> "Left side wins
+
+// My solution :
+
+public class AlphabetWarAirstrike{
+
+  public static String alphabetWar(String fight){
+    
+    boolean[] isDestroy = new boolean[fight.length()];
+    
+    for (int i = 0; i < fight.length(); i++) {
+      
+      if (fight.charAt(i) == '*') {
+        isDestroy[i] = true;
+        if (i > 0) {
+          isDestroy[i - 1] = true;
+        }
+        if (i < fight.length() - 1) {
+          isDestroy[i + 1] = true;
+        }
+      }
+      
+    }
+        
+    char[] left = {'s', 'b', 'p', 'w'};
+    char[] right = {'z', 'd', 'q', 'm'};
+    int leftPoints = 0;
+    int rightPoints = 0;
+    
+    for (int i = 0; i < fight.length(); i++) {
+      if (isDestroy[i] == false) {    
+        
+        for (int s = 0; s < left.length; s++) {
+          if (fight.charAt(i) == left[s]) {
+            leftPoints += s + 1;
+          }
+        }
+        for (int s = 0; s < right.length; s++) {
+          if (fight.charAt(i) == right[s]) {
+            rightPoints += s + 1;
+          }
+        }
+        
+      }
+    }
+    
+    if (leftPoints == rightPoints) {
+      return "Let's fight again!";
+    } else if (leftPoints > rightPoints) {
+      return "Left side wins!";
+    } else {
+      return "Right side wins!";
+    }
+    
+  }
+}
